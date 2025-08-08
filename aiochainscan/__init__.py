c33858ddc6c8932839024e7d855827b1f36934eb
VaitaR <andrey.shivalin@gmail.com>
2025-08-08 15:20:59 +0300

    Add facade get_balance() using default aiohttp adapter; extend HttpClient with aclose()

--- aiochainscan/__init__.py
+++ aiochainscan/__init__.py
@@ -1,3 +1,4 @@
+from aiochainscan.adapters.aiohttp_client import AiohttpClient
 from aiochainscan.client import Client  # noqa: F401
 from aiochainscan.config import ChainScanConfig, ScannerConfig, config  # noqa: F401
 from aiochainscan.domain.models import Address, BlockNumber, TxHash  # re-export domain VOs
@@ -15,3 +16,22 @@ __all__ = [
     # Services (facade)
     'get_address_balance',
 ]
+
+
+async def get_balance(*, address: str, api_kind: str, network: str, api_key: str) -> int:
+    """Fetch address balance using the default aiohttp adapter.
+
+    Convenience facade for simple use without manual client wiring.
+    """
+
+    http = AiohttpClient()
+    try:
+        return await get_address_balance(
+            address=Address(address),
+            api_kind=api_kind,
+            network=network,
+            api_key=api_key,
+            http=http,
+        )
+    finally:
+        await http.aclose()
